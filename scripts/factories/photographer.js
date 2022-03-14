b59c7805235e352395c8552f3175df3922e5a98e
Joris Monleon <monleon.joris@gmail.com>
2022-03-14 14:09:43 +0100

    Rework index.js + factoPhotographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,38 @@
+
 function photographerFactory(data) {
-  const { name, portrait } = data;
+  const { name, portrait, country, tagline, id } = data;
 
   const picture = `assets/photographers/${portrait}`;
+  const href = `photographer.html?id=${id}`;
 
   function getUserCardDOM() {
     const article = document.createElement('article');
+
+    const link = document.createElement('a');
+    link.setAttribute("href", href)
+
     const img = document.createElement('img');
     img.setAttribute("src", picture)
+
     const h2 = document.createElement('h2');
     h2.textContent = name;
-    article.appendChild(img);
+
+    const p = document.createElement('p');
+    p.textContent = country;
+
+    const p2 = document.createElement('p');
+    p2.textContent = tagline;
+
+    link.appendChild(img);
+    article.appendChild(link);
     article.appendChild(h2);
+    article.appendChild(p);
+    article.appendChild(p2);
+
     return (article);
   }
+
   return { name, picture, getUserCardDOM }
 }
+
+export default photographerFactory;
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,13 @@
-async function getPhotographers() {
-  // Penser à remplacer par les données récupérées dans le json
-  const photographers = [
-    {
-      "name": "Ma data test",
-      "id": 1,
-      "city": "Paris",
-      "country": "France",
-      "tagline": "Ceci est ma data test",
-      "price": 400,
-      "portrait": "account.png"
-    },
-    {
-      "name": "Autre data test",
-      "id": 2,
-      "city": "Londres",
-      "country": "UK",
-      "tagline": "Ceci est ma data test 2",
-      "price": 500,
-      "portrait": "account.png"
-    },
-  ]
-  // et bien retourner le tableau photographers seulement une fois
-  return ({
-    photographers: [...photographers, ...photographers, ...photographers]
-  })
+import photograph from '../data/photographers.js';
+import photographerFactory from '../factories/photographer.js'
+
+
+main();
+
+function main() {
+  const photographers = photograph;
+  console.log(photographers);
+  displayData(photographers);
 }
 
 async function displayData(photographers) {
@@ -36,10 +20,35 @@ async function displayData(photographers) {
   });
 };
 
-async function init() {
-  // Récupère les datas des photographes
-  const { photographers } = await getPhotographers();
-  displayData(photographers);
-};
 
-init();
+
+
+
+//###################################################################################
+
+// async function getPhotographers() {
+//   const photographers = [photograph];
+//   console.log(photograph);
+
+//   return ({
+//     photographers: [...photographers]
+//   })
+// }
+
+// async function displayData(photographers) {
+//   const photographersSection = document.querySelector(".photographer_section");
+
+//   photographers.forEach((photographer) => {
+//     const photographerModel = photographerFactory(photographer);
+//     const userCardDOM = photographerModel.getUserCardDOM();
+//     photographersSection.appendChild(userCardDOM);
+//   });
+// };
+
+// async function main() {
+//   // Récupère les datas des photographes
+//   const { photographers } = await getPhotographers();
+//   displayData(photographers);
+// };
+
+// main();
