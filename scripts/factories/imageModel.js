3ded019ef0e292e10da992491a3af95b882389c0
Joris Monleon <monleon.joris@gmail.com>
2022-03-14 15:29:59 +0100

    Init of the factory

---
+++ scripts/factories/imageModel.js
@@ -0,0 +1,29 @@
+class Image {
+  constructor(media) {
+    this.id = media.id;
+    this.photographerId = media.photographerId;
+    this.title = media.title;
+    this.likes = media.likes;
+    this.date = media.date;
+  }
+
+  get id() {
+
+  }
+
+  get photographerId() {
+
+  }
+
+  get title() {
+
+  }
+
+  get likes() {
+
+  }
+
+  get date() {
+
+  }
+}
