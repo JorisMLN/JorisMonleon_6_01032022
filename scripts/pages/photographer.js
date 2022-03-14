04115872bd920c5bc98cc48f9959a6c982598190
Joris Monleon <monleon.joris@gmail.com>
2022-03-14 14:09:15 +0100

    Add main photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,11 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import media from '../data/media.js';
+
+main();
+
+function main() {
+  console.log(media);
+
+  let photographerParameters = window.location.search;
+  let photographerID = photographerParameters.split('=')[1];
+  console.log(photographerID);
+}
