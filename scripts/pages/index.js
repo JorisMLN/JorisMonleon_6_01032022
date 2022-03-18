68c885303ffd32fcfa7d8e0a8988a965300b1d1f
JorisMLN <monleon.joris@gmail.com>
2022-03-18 11:28:39 +0100

    Header dynamic info

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -19,36 +19,3 @@ async function displayData(photographers) {
     photographersSection.appendChild(userCardDOM);
   });
 };
-
-
-
-
-
-//###################################################################################
-
-// async function getPhotographers() {
-//   const photographers = [photograph];
-//   console.log(photograph);
-
-//   return ({
-//     photographers: [...photographers]
-//   })
-// }
-
-// async function displayData(photographers) {
-//   const photographersSection = document.querySelector(".photographer_section");
-
-//   photographers.forEach((photographer) => {
-//     const photographerModel = photographerFactory(photographer);
-//     const userCardDOM = photographerModel.getUserCardDOM();
-//     photographersSection.appendChild(userCardDOM);
-//   });
-// };
-
-// async function main() {
-//   // Récupère les datas des photographes
-//   const { photographers } = await getPhotographers();
-//   displayData(photographers);
-// };
-
-// main();
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,5 +1,6 @@
 import media from '../data/media.js';
 import photograph from '../data/photographers.js';
+// import MediaFactory from '../factories/mediaFactory';
 
 main();
 
@@ -9,6 +10,7 @@ function main() {
   let photographerID = window.location.search.split('=')[1];
 
   const photographerFiltered = photograph.filter(filterOptions);
+  console.log(photographerFiltered);
 
   function filterOptions(item) {
     if (item.id === Number(photographerID)) {
@@ -17,6 +19,46 @@ function main() {
       return false;
     }
   }
-
-  console.log(photographerFiltered);
+  
+  headerSettings(photographerFiltered);
 }
+
+function headerSettings(settings){
+
+  let profil = {
+    name: '',
+    city: '',
+    country: '',
+    photo: '',
+    tagline: ''
+  }
+
+  settings.forEach((item) => {
+    profil.name = item.name;
+    profil.city = item.city;
+    profil.country = item.country;
+    profil.photo = item.portrait;
+    profil.tagline = item.tagline;
+  })
+
+  console.log(profil);
+  
+  let leftSide = document.getElementById('headerLeft');
+  let leftHtml = `
+    <div class='test'>${profil.name}</div>
+    <div class='test'>${profil.city} ${profil.country}</div>
+    <div class='test'>${profil.tagline}</div>
+  `;
+  leftSide.innerHTML = leftHtml;
+
+
+  let rightSide = document.getElementById('headerRight');
+  let rightHtml = `
+    <img class='test' src='assets/photographers/${profil.photo}'></img>
+  `;
+  rightSide.innerHTML = rightHtml;
+}
+
+// function mediaDisplay(){
+
+// }
